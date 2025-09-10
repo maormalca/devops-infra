caaf78676b6b02bdb7d9b130f257d169d8238162
maormalca <maor16001@gmail.com>
2025-09-10 22:27:45 +0300

    add notes

--- main.tf
+++ main.tf
@@ -13,25 +13,20 @@ provider "aws" {
   region = var.region
 }
 
-
-# ECR Repository
-
+# Creates an ECR repository for storing Docker images
 resource "aws_ecr_repository" "counter_service" {
-  name                 = "counter-service-repo"
+  name                  = "counter-service-repo"
   image_tag_mutability = "MUTABLE"
 }
 
-# EKS cluster
-
+# EKS cluster using a module
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
   name               = var.cluster_name
   kubernetes_version = "1.33"
-
   endpoint_public_access = true
-
   enable_cluster_creator_admin_permissions = true
 
   compute_config = {
@@ -43,8 +38,7 @@ module "eks" {
   subnet_ids = var.subnets
 }
 
-
-
+# Outputs for use in CI/CD or kubectl configuration
 output "cluster_name" {
   value = module.eks.cluster_name
 }
@@ -59,4 +53,4 @@ output "cluster_certificate_authority_data" {
 
 output "ecr_repository_url" {
   value = aws_ecr_repository.counter_service.repository_url
-} 
+}
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "eu-north-1" // Stockholm
+  default     = "eu-north-1"
 }
 
 variable "cluster_name" {
